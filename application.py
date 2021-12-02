543736cdd81dcb637ccceb812eae6e7ef3d5690e
Ramsai170899 <81312213+Ramsai170899@users.noreply.github.com>
2021-12-02 22:30:29 +0530

    Update application.py

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ body = st.beta_container()
 classify_container = st.beta_container()
 
 def classify(a):
-    filename = 'sgdmodel_.pkl'
+    filename = 'pipe4_pickle.pkl'
     model_reloaded = pickle.load(open(filename, 'rb'))
     
     te =[]
