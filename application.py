7badf96a016f5ea9ccf669fd11e7aec1d89288dd
Ramsai170899 <81312213+Ramsai170899@users.noreply.github.com>
2021-12-02 22:49:25 +0530

    Update application.py

--- application.py
+++ application.py
@@ -10,14 +10,6 @@ header = st.beta_container()
 body = st.beta_container()
 classify_container = st.beta_container()
 
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-background-size: cover;
-}
-</style>
-'''
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
@@ -51,7 +43,15 @@ with header:
 
 with body:
     rawtext = st.text_area('Enter Text Here')
-  
+    
+  page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
 
     uploaded_file = st.file_uploader(
         'Choose your .txt file', type="txt")
